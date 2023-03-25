622b5f8fb70b06b4a7fca395fc20c781e363b0a5
andrey.nascimento4@etec.sp.gov.br <andrey.nascimento4@etc.sp.gov.br>
2023-03-24 22:04:02 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int n1,n2;
+
+Console.Write("Digite um número: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite outro  número: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+int quociente, resto;
+
+quociente = n1 / n2;
+resto = n1 % n2;
+
+Console.Write($" O quociente entre {n1} e {n2}");
+
+// ":n3" ele mostra as casinhas que vai a virgula
+Console.WriteLine($" é {quociente:n3} e sobra {resto:n3}");
+
